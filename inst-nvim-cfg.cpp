f5ca7f4673335621d7cfeccd472a1b5bf0880151
D179090 <102134977+D179090@users.noreply.github.com>
2023-01-03 22:45:53 +0300

    Add files via upload

---
+++ inst-nvim-cfg.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+		cout << "Вы хотите установить конфиг neovim? (yes or no)" << endl;
+		int a;
+		cin >> a;
+		if (a == yes){
+				system("git clone https://github.com/D179090/neovim-config");
+				system("mv vimrc .vimrc");
+				system("mkdir -p .config/nvim");
+				system("mv neovim-config/.vimrc .config/nvim/");
+				system("mv neovim-config/init.vim .config/nvim/");
+				cout << "Теперь запустите nvim и введите команду Plug-install" << endl;
+				cout << "На этом всё спасибо за установку" << endl;
+		}
+		else(a == no){
+				cout << "Выход" << endl;
+		}
+		if (){
+				cout << "Не правильное действие" << endl;
+		}
+}
