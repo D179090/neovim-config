c561d65854fe2d84f0eac3c478005197c502f811
D179090 <102134977+D179090@users.noreply.github.com>
2023-01-03 10:07:25 +0300

    Add files via upload

---
+++ install-nvim.py
@@ -0,0 +1,10 @@
+import os
+
+print("Установка начата")
+os.system(https://github.com/D179090/neovim-config)
+os.system(cd neovim-config)
+os.rename(vimrc,.vimrc)
+os.system(cd)
+os.system(mkdir .config/nvim)
+os.system(mv neovim-config/init.vim .config/nvim/)
+print("Установка заверщена")
